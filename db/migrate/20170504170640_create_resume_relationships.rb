05c5a60365b0f4eb7b4a51dc6078189492291df8
wuxiao <wudawuxiao1375@163.com>
2017-05-05 01:13:27 +0800

    g resume_relationship

--- db/migrate/20170504170640_create_resume_relationships.rb
+++ db/migrate/20170504170640_create_resume_relationships.rb
@@ -2,7 +2,6 @@ class CreateResumeRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :resume_relationships do |t|
       t.integer :job_id
-      t.integer :resume_id
       t.integer :user_id
 
       t.timestamps
